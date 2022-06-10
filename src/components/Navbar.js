af8f6be77514c1ecb6b2568b68b786a1122c9590
samx03 <sameerbakshi34@gmail.com>
2022-06-10 23:06:47 +0530

    Improved dark mode functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
+
     return (
-        <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
+        <nav className={`navbar navbar-expand-lg  navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,6 +19,11 @@ export default function Navbar(props) {
                             <a className="nav-link" href="/">{props.aboutText}</a>
                         </li>
                     </ul>
+
+                    <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable {props.mode === "dark"?"light":"dark"} Mode</label>
+                    </div>
                 </div>
             </div>
         </nav>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,12 +21,12 @@ export default function TextForm(props) {
         setText(newText)
     }
 
-    const handleReverseClick = ()=>{
+    const handleReverseClick = () => {
         let newText = text.split(" ").reverse().join(" ");
         setText(newText)
     }
 
-    const handleCopy = ()=>{
+    const handleCopy = () => {
         let text = document.getElementById("myBox")
         text.select()
         navigator.clipboard.writeText(text.value)
@@ -38,9 +38,9 @@ export default function TextForm(props) {
     }
     return (
         <>
-            <div className="mb-3">
+            <div className="mb-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h3>{props.heading}</h3>
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? '#042743' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert To Lowercase</button>
@@ -48,12 +48,12 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-2" onClick={handleReverseClick}>Reverse Text</button>
             <button className="btn btn-primary" onClick={handleCopy}>Copy Text</button>
 
-            <div className="container my-5">
+            <div className="container my-5" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>Your text summary</h1>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} Minute read.</p>
                 <h2> Preview </h2>
-                <p>{text}</p>
+                <p>{text.length>0?text:"Enter something in the textbox above to preview it here."}</p>
             </div>
         </>
     )
