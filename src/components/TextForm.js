531c1e0e2438e9dd89d06bf484a7f495ed33f19b
samx03 <sameerbakshi34@gmail.com>
2022-06-11 20:24:07 +0530

    Fixed a bug

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -55,10 +55,10 @@ export default function TextForm(props) {
 
             <div className="container my-5" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>Your text summary</h1>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{text.length > 0 ? text.trim().split(" ").length : 0} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} Minute read.</p>
                 <h2> Preview </h2>
-                <p>{text.length>0?text:"Enter something in the textbox above to preview it here."}</p>
+                <p>{text.length > 0 ? text : "Enter something in the textbox above to preview it here."}</p>
             </div>
         </>
     )
