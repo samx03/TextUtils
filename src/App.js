c03dd4519ab048c246335e2807f93dc137647e26
samx03 <sameerbakshi34@gmail.com>
2022-06-12 14:23:04 +0530

    Added custom color themes

--- src/App.js
+++ src/App.js
@@ -18,16 +18,28 @@ function App() {
       setAlert(null)
     }, 2000);
   }
-  const toggleMode = () => {
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    document.body.classList.add('bg-' + cls)
+    const capitalize = (word)=>{
+      let lower = word.toLowerCase();
+      return lower.charAt(0).toUpperCase() + lower.slice(1);
+  }
     if (mode === 'light') {
       setMode('dark')
-      document.body.style.backgroundColor = "#042743"
-      showAlert("Dark mode has been enabled", "success")
+      showAlert(capitalize(cls) + " mode has been enabled", "success")
     } else {
       setMode('light')
-      document.body.style.backgroundColor = "white"
       showAlert("Light mode has been enabled", "success")
     }
+  }
+
+  const removeBodyClasses = () => {
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-dark');
+    document.body.classList.remove('bg-danger');
+    document.body.classList.remove('bg-success');
+    document.body.classList.remove('bg-warning');
   }
   return (
     <>
